f0e0e77ee1c1c4cbf947591d1c1c76b86be46c30
shayandaneshvar <daneshvarshayan@gmail.com>
2019-09-28 23:17:45 +0330

    Initial Commit

---
+++ src/main/java/ir/shayandaneshvar/springwebfluxrestexample/SpringWebfluxRestExampleApplication.java
@@ -0,0 +1,13 @@
+package ir.shayandaneshvar.springwebfluxrestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxRestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxRestExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/shayandaneshvar/springwebfluxrestexample/SpringWebfluxRestExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package ir.shayandaneshvar.springwebfluxrestexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebfluxRestExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
